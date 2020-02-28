ea5d7c33e39b8f61385d99133efb1c12ff035654
Nick Lewis <nickeblewis@gmail.com>
2020-02-28 12:26:16 +0000

    First commit

---
+++ src/layouts/sections/index/HeaderBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="height-30 bg-dark text-white jarallax">
+    <g-image
+      src="~/resources/images/marvin-meyer-SYTO3xs06fU-unsplash.jpg"
+      class="jarallax-img opacity-40"
+      alt="ITSMPlus"
+    />
+    <b-container>
+      <b-row class="justify-content-center text-center">
+        <b-col md="10" lg="8" class="pt-3 pb-3">
+          <p class="display-4 mb-0">Rushmoor Arts Festival</p>
+          <p class="lead">Do we have a slogan for the festival, if we do it should go here!</p>
+        </b-col>
+
+        
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
